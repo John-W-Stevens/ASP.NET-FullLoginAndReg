15b254505a49f2cdc6c9735da86cd31e73c48cf4
John Stevens <john.william.stevens1@gmail.com>
2020-06-11 11:56:13 -0700

    initial commit

---
+++ Models/LRContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginAndRegistration.Models
+{
+    public class LRContext : DbContext
+    {
+        public LRContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/LoginUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginAndRegistration.Models
+{
+    public class LoginUser
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string LoginEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string LoginPassword { get; set; }
+    }
+}
